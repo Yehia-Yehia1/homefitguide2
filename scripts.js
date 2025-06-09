1846bb3c39d8cd04ecfa96f32ad86b30759c901b
Yehia-Yehia1 <yehia.yehia.pt@gmail.com>
2025-06-09 13:07:53 +0300

    article + Search bar

--- scripts.js
+++ scripts.js
@@ -12,3 +12,18 @@ document.addEventListener('click', function (event) {
         sideMenu.classList.remove('active');
     }
 });
+function searchArticles() {
+    const input = document.getElementById("searchInput").value.toLowerCase();
+    const articles = document.querySelectorAll(".featured-article");
+
+    articles.forEach(article => {
+        const title = article.querySelector("h2").innerText.toLowerCase();
+        const altText = article.querySelector("img").alt.toLowerCase();
+
+        if (title.includes(input) || altText.includes(input)) {
+            article.style.display = "block";
+        } else {
+            article.style.display = "none";
+        }
+    });
+}
